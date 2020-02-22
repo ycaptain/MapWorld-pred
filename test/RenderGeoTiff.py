5ff4624b379c4b6c5a3e4b5d950bbc1142adacfa
Tong Ge <comword@live.com>
2020-02-23 01:42:13 +0800

    chore(rm_opencv): Removed OpenCV dependency. use lightweight PIL image library to save and preview images.
    feat(geojson_raster): use gdal library to raster geojson vectors to images.
    
    refactor(GeoLabelUtil, GeoTest): clearify, remove data_dir in __init__,
    resolve the path in caller, should give full path in load_* functions.

--- test/RenderGeoTiff.py
+++ test/RenderGeoTiff.py
@@ -1,5 +1,6 @@
 import os, sys, threading
 from pathlib import Path
+import re
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 src_dir = os.path.join(root_dir, "src")
 sys.path.insert(0, src_dir)
@@ -9,30 +10,56 @@ os.chdir(root_dir)
 from parse_config import ConfigParser
 from utils import util_geo
 
+re_img_index = re.compile("img\d+")
+
 
 class RenderThread(threading.Thread):
-    def __init__(self, imgdir, name, path):
+    def __init__(self, img_src, tg_img, geojson_src=None, ras_img=None):
+        """
+        Args: img_src: Source image path (Raw satellite image)
+              tg_img: target image (RGB) output path
+              geojson_src:
+              resimg_path: raster image (geojson mask) output path
+        """
         threading.Thread.__init__(self)
         util = util_geo.GeoLabelUtil()
-        self.util = util.GeoImgUtil(imgdir)
-        self.name = name
-        self.path = path
+        self.util = util.GeoImgUtil()
+        self.img_src = img_src
+        self.tg_img = tg_img
+        if geojson_src is not None and ras_img is not None:
+            self.geojson_src = geojson_src
+            self.ras_img = ras_img
+            self.geojson_util = util.GeoJsonUtil()
+            self.do_raster = True
 
     def run(self):
-        img = self.util.load_geotiff(self.name)
-        self.util.save(img, self.name, self.path)
-        print(self.name, "saved to ", self.path)
+        img = self.util.load_geotiff(self.img_src)
+        rimg = self.util.normalize_img(img.ReadAsArray())
+        self.util.save(rimg, self.tg_img)
+        print("RGB saved to", self.tg_img)
+        if self.do_raster:
+            f_load = self.geojson_util.load_geojson(self.geojson_src)
+            res = self.geojson_util.render(self.util.read_meta(img), f_load.GetLayer())
+            self.util.save(res, self.ras_img)
+            print("Mask saved to", self.ras_img)
 
 
 if __name__ == '__main__':
     config = ConfigParser(ConfigParser.from_file("test/configs/geotest.json"))
     logger = config.get_logger('train')
     data_dir = Path(config['data_loader']['args']['data_dir'])
-    save_dir = Path(config["trainer"]["save_dir"]) / "data"
+    img_save_dir = Path(config["trainer"]["save_dir"]) / "data" / "RGB"
     data_name = config['data_loader']['args']['data_name']
+    img_dir = data_dir / data_name / "RGB-PanSharpen"
 
-    imgdir = data_dir / data_name / "RGB-PanSharpen"
-    save_dir.mkdir(parents=True, exist_ok=True)
-    for f in os.listdir(imgdir):
-        thread = RenderThread(imgdir, f, save_dir)
+    building_geojson_dir = data_dir / data_name / "geojson" / "buildings"
+    roads_geojson_dir = data_dir / data_name / "geojson" / "roads"
+    raster_save_dir = Path(config["trainer"]["save_dir"]) / "data" / "labels"
+
+    img_save_dir.mkdir(parents=True, exist_ok=True)
+    raster_save_dir.mkdir(parents=True, exist_ok=True)
+    for f in os.listdir(img_dir):
+        img_index = re_img_index.search(f).group(0)
+        geojson_fname = "buildings_%s%s.geojson" % (data_name.replace("Train", "").replace("Test", ""), img_index)
+        thread = RenderThread(img_dir / f, img_save_dir / f, building_geojson_dir / geojson_fname, raster_save_dir / f)
         thread.run()
